b566ebd4a1d996f47f2e0ffbb8a7bc33fdf74d86
PLKuba <major.plays96@gmail.com>
2022-01-07 22:08:29 +0100

    add tests

--- test_database_operations.py
+++ test_database_operations.py
@@ -26,22 +26,51 @@ with open(file="DATA/wiki_data.json") as f:
 
 redirections = json.dumps(page_json)
 
-sql = """SELECT id FROM wikipedia WHERE wikipedia_title=%s"""
+"""redirections"""
+# get id where redirections are inserted
+get_id_by_redirections_title = """SELECT id FROM wikipedia WHERE title_to_match_redirections=%s"""
 
-# sql = """INSERT INTO wikipedia (database_title, wikipedia_title)
-#             VALUES(%s, %s)
-#             ON CONFLICT (id)
-#                 DO
-#                     UPDATE SET
-#                     redirections = %s"""
+# insert redirections to db with titles in it
+insert_redirections = """INSERT INTO wikipedia (redirections, title_to_match_redirections)
+        VALUES(%s, %s)"""
 
-# cur.execute(sql , ('test_title1', 'test_title2', redirections))
+update_redirections = """INSERT INTO wikipedia (id)
+                                VALUES(%s)
+                                ON CONFLICT (id)
+                                    DO
+                                        UPDATE SET
+                                        redirections=%s,
+                                        title_to_match_redirections=%s"""
 
-cur.execute(sql , ('test_title2',))
+"""titles"""
+# get id where titles are inserted
+get_id_by_title = """SELECT id FROM wikipedia WHERE wikipedia_title=%s OR wikipedia_title=%s"""
+
+insert_titles = """INSERT INTO wikipedia (database_title, wikipedia_title)
+        VALUES(%s, %s)"""
+
+# insert titles to db with redirections in it
+update_tiles = """INSERT INTO wikipedia (id)
+                                VALUES(%s)
+                                ON CONFLICT (id)
+                                    DO
+                                        UPDATE SET
+                                        database_title=%s,
+                                        wikipedia_title=%s"""
+
+# cur.execute(sql , ('db_tytul', 'wiki_tytul'))
+cur.execute(get_id_by_title, ('dupa123', 'dupa456',))
 
 r = cur.fetchone()
+print(r)
+if r is None:
+    print("LOL JESTES NONE")
 id = r[0]
-print('Table id: ',id)
+print(type(id))
+print(bytes(str(id), encoding='utf-8'))
+
+cur.execute(update_tiles, (id, 'dupa123', 'dupa456',))
+
 # r = cur.fetchone()
 # print(r)
 
