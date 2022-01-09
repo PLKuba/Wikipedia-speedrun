e141b226dccde740fd82be84cf5b4e8502be795c
PLKuba <major.plays96@gmail.com>
2022-01-09 19:07:33 +0100

    update schema

--- sql_scripts/create_wikipedia_table.sql
+++ sql_scripts/create_wikipedia_table.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS wikipedia;
 
 CREATE TABLE wikipedia (
-    id                  SERIAL PRIMARY KEY,
+    id                              SERIAL PRIMARY KEY,
     database_title                  varchar(255),
     wikipedia_title                 varchar(255),
     title_to_match                  varchar(255),
