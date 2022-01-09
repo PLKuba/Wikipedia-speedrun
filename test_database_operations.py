5a6e528be4ff9c0d33cf6e0b182749d603099dbe
PLKuba <major.plays96@gmail.com>
2022-01-09 19:08:43 +0100

    test fetching data

--- test_database_operations.py
+++ test_database_operations.py
@@ -26,50 +26,33 @@ with open(file="DATA/wiki_data.json") as f:
 
 redirections = json.dumps(page_json)
 
-"""redirections"""
-# get id where redirections are inserted
-get_id_by_redirections_title = """SELECT id FROM wikipedia WHERE title_to_match_redirections=%s"""
-
-# insert redirections to db with titles in it
-insert_redirections = """INSERT INTO wikipedia (redirections, title_to_match_redirections)
-        VALUES(%s, %s)"""
-
-update_redirections = """INSERT INTO wikipedia (id)
-                                VALUES(%s)
-                                ON CONFLICT (id)
-                                    DO
-                                        UPDATE SET
-                                        redirections=%s,
-                                        title_to_match_redirections=%s"""
-
-"""titles"""
-# get id where titles are inserted
-get_id_by_title = """SELECT id FROM wikipedia WHERE wikipedia_title=%s OR wikipedia_title=%s"""
-
-insert_titles = """INSERT INTO wikipedia (database_title, wikipedia_title)
-        VALUES(%s, %s)"""
-
-# insert titles to db with redirections in it
-update_tiles = """INSERT INTO wikipedia (id)
-                                VALUES(%s)
-                                ON CONFLICT (id)
-                                    DO
-                                        UPDATE SET
-                                        database_title=%s,
-                                        wikipedia_title=%s"""
-
 # cur.execute(sql , ('db_tytul', 'wiki_tytul'))
-cur.execute(get_id_by_title, ('dupa123', 'dupa456',))
+# cur.execute(get_id_by_title, ('dupa123', 'dupa456',))
 
-r = cur.fetchone()
-print(r)
-if r is None:
-    print("LOL JESTES NONE")
-id = r[0]
-print(type(id))
-print(bytes(str(id), encoding='utf-8'))
+sql = """SELECT redirections FROM wikipedia WHERE title_to_match = %s"""
 
-cur.execute(update_tiles, (id, 'dupa123', 'dupa456',))
+cur.execute(sql, (
+    'communications in afghanistan',
+))
+
+red_array = cur.fetchone()[0]
+
+for item in red_array:
+    sql = """SELECT redirections FROM wikipedia WHERE title_to_match = %s"""
+
+    cur.execute(sql, (
+        item,
+    ))
+
+    res = cur.fetchone()
+
+    if res is not None:
+        red_array = res[0]
+        print(item)
+
+        print(red_array, '\n')
+
+# cur.execute(update_tiles, (id, 'dupa123', 'dupa456',))
 
 # r = cur.fetchone()
 # print(r)
