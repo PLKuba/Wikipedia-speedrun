b1cc4a5a285b9ce7bfa9d452139169f48923fb98
PLKuba <major.plays96@gmail.com>
2022-04-01 12:12:39 +0200

    update db

--- playground.py
+++ playground.py
@@ -1,4 +1,5 @@
 import json
+import os
 from lxml import etree
 from io import BytesIO
 import re
@@ -14,16 +15,22 @@ import itertools
 import CONFIG.config
 
 
+# shell command to read lines in a file (sed gives an exact result unlike 'wc')
+READ_LINES_COMMAND = "sed -n '$=' DATA/test.xml"
 # total lines in wiki file
 LINE_COUNT = 1_288_367_801
 # wikipedia file path
 WIKI_FILE_PATH = 'DATA/enwiki-20211220-pages-articles-multistream.xml'
 # wikipedia test file path
 TEST_FILE_PATH = 'DATA/test.xml'
-# regex to match redirections in page eg. "[[xyz]]"
+# regex to match the following: [[xyz]]
 REDIRECTION_REGEX = r'\[\[[^\]^\[]{1,}\]\]'
-# regex to match redirect title in page eg. "<redirect title="David Talbot" />\n"
-REDIRECT_TITLE_REGEX = r'<redirection_title = ".{0,255}">'
+# regex to match the following: <redirect title="David Talbot" />\n
+REDIRECT_TITLE_REGEX = r'<redirect title=".{0,255}">'
+# regex to match the following: <title>AccessibleComputing</title>
+TITLE_REGEX = r''
+# regex to match the following: <text bytes="111" xml:space="preserve">#REDIRECT [[Computer accessibility]]
+REDIRECT_REGEX = r''
 # max number of threads
 MAX_THREADS = 30
 
@@ -48,14 +55,21 @@ def measure_time(func):
     return dec_inner
 
 
-def insert_pages_to_db(cur, page_limit=None,):
+# function to update database with all wikipedia content
+def insert_pages_to_db(cur, page_limit=None, count_lines=False):
     logging.warning(f"Inserting {page_limit} pages to DB")
+
     with open(file=WIKI_FILE_PATH, mode='r') as file:
         total_pages = 1
 
         count = 0
 
         # while count <= LINE_COUNT and (page_limit is None or total_pages <= page_limit):
+
+        if count_lines:
+            return_value = os.popen(READ_LINES_COMMAND).read()
+            LINE_COUNT = int(return_value[:-1])
+
         while count <= LINE_COUNT:
             try:
                 line = file.readline()
@@ -63,7 +77,6 @@ def insert_pages_to_db(cur, page_limit=None,):
                 # skip blank lines
                 if not bool(line.strip()):
                     count+=1
-
                     continue
 
                 lines_arr = []
@@ -88,16 +101,84 @@ def insert_pages_to_db(cur, page_limit=None,):
                 ))
 
                 conn.commit()
+                # conn.close()
 
             except (TypeError, IndexError, KeyError, AttributeError) as e:
                 logging.exception(e)
 
-    # print("Succesfully updated database")
+    print("Succesfully updated database")
+
+
+def parse_redirections(redirections):
+    redirections2 = {
+        'links':[],
+        'files': [],
+        'categories': [],
+        'wikipedia_exclusive': [],
+        'count':len(redirections)
+    }
+
+    for item in redirections:
+        if item.startswith('category:'):
+            val1 = item[len('category:'):]
+
+            # redirections2.append((val1, None))
+            redirections2["categories"].append(val1)
+            redirections2["links"].append(val1)
+
+        if item.startswith(':category:'):
+            val1 = item[len(':category:'):]
+
+            redirections2["categories"].append(val1)
+            redirections2["links"].append(val1)
+
+        elif '|' in item:
+            val1 = item[item.index('|') + 1:]
+            val2 = item[:item.index('|')]
+
+            # redirections2.append((val1, val2))
+            redirections2['links'].append(val1)
+
+        elif item.startswith('wikipedia:'):
+            redirections2["wikipedia_exclusive"].append(item)
+
+        elif item.startswith('file:'):
+            redirections2["files"].append(item)
+
+        else:
+            redirections2['links'].append(item)
+
+    return redirections2
+    # return json.dumps(redirections2)
 
 
 # function to fetch wikipedia titles and database titles
-def update_db_with_page_values(cur, page_lines):
+def update_db_with_page_content(cur, page_lines):
+    titles = []
+
     try:
+        # check if this is just a redirect page
+        for item in page_lines:
+            try:
+                # Titles:
+                # <title>AccessibleComputing</title>
+                # titles.append(re.search(TITLE_REGEX, item).string[17:-4])
+
+                # <redirect title="Computer accessibility" />
+                content = re.search(REDIRECT_TITLE_REGEX, item).string[17:-4]
+                # print(content)
+                titles.append(content)
+
+                # <text bytes="111" xml:space="preserve">#REDIRECT [[Computer accessibility]]
+                # titles.append(re.search(REDIRECT_REGEX, item).string[17:-4])
+
+                # TODO: find all titles and store them in an array or smth, there should not be only one source or a title like string
+                # Variety of those is too much
+
+                # return (title_to_match_redirections, title_to_match_redirections, None, None)
+            except (TypeError, IndexError, KeyError, AttributeError):
+                pass
+
         # making a iterparse_lxml object from array
         parser = etree.XMLParser()
 
@@ -112,7 +193,7 @@ def update_db_with_page_values(cur, page_lines):
 
         wikipedia_title = None
 
-        redirections = None
+        redirections_json = None
 
         title_to_match_redirections = None
 
@@ -132,7 +213,13 @@ def update_db_with_page_values(cur, page_lines):
                             break
 
                         else:
-                            redirections = json.dumps([redirection[2:-2].lower() for redirection in match])
+                            redirections = [redirection[2:-2].lower() for redirection in match]
+
+                            redirections = parse_redirections(redirections)
+
+                            # print(redirections)
+
+                            redirections_json = redirections
 
                             title_to_match_redirections_f = lambda: [element.text.lower() for event, element in etree.iterparse(BytesIO(etree.tostring(root))) if element.tag.strip().lower() == 'title']
 
@@ -141,12 +228,13 @@ def update_db_with_page_values(cur, page_lines):
             except (TypeError, IndexError, KeyError, AttributeError, IndexError) as e:
                 logging.exception(e)
 
-        # print(title_to_match_redirections, redirections)
+        # print(title_to_match_redirections, redirections_json)
         # print(wikipedia_title, database_title)
 
+        # print(database_title, wikipedia_title, title_to_match_redirections, redirections_json)
         # inserting data into db
         if database_title is not None and wikipedia_title is not None:
-            # print("wikipedia_title: {0}\ndatabase_title: [{1}, ...]\n".format(wikipedia_title, database_title))
+            print("wikipedia_title: {0}\ndatabase_title: [{1}, ...]\n".format(wikipedia_title, database_title))
 
             cur.execute(CONFIG.config.get_id_by_title_titles, (
                 wikipedia_title,
@@ -171,8 +259,8 @@ def update_db_with_page_values(cur, page_lines):
                     wikipedia_title,
                 ))
 
-        if title_to_match_redirections is not None and redirections is not None:
-            # print("title_to_match_redirections: {0}\nredirections: [{1}, ...]\n".format(title_to_match_redirections, redirections.split(',')[0]))
+        if title_to_match_redirections is not None and redirections_json is not None:
+            print("title_to_match_redirections: {0}\nredirections_json: [{1}, ...]\n".format(title_to_match_redirections, json.dumps(redirections_json, indent=4)))
 
             cur.execute(CONFIG.config.get_id_by_title_redirections, (
                 title_to_match_redirections,
@@ -182,7 +270,7 @@ def update_db_with_page_values(cur, page_lines):
 
             if res is None:
                 cur.execute(CONFIG.config.insert_redirections, (
-                    redirections,
+                    redirections_json,
                     title_to_match_redirections,
                 ))
 
@@ -191,39 +279,60 @@ def update_db_with_page_values(cur, page_lines):
 
                 cur.execute(CONFIG.config.update_redirections, (
                     id,
-                    redirections,
+                    redirections_json,
                 ))
 
+        conn.commit()
+
+        return (title_to_match_redirections, wikipedia_title, database_title, redirections_json)
 
     except (TypeError, IndexError, KeyError, AttributeError, IndexError) as e:
         logging.exception(e)
 
 
 @measure_time
-def main(cur, step=1000, start_id=0):
+def main(cur, step=2, start_id=0, LIMIT=LINE_COUNT):
     try:
         if step is not None:
             cur.execute(CONFIG.config.get_range_pages_from_db, (
-                step,
-                start_id,
+                        step,
+                        start_id,
             ))
-
+            print(start_id)
             res = cur.fetchall()
+            # print(res)
+            # return
 
             start_id += step
 
+            futures = []
+
+            # while bool(res) and start_id<=LIMIT:
             while bool(res):
+                print(start_id)
                 with ThreadPoolExecutor(max_workers=MAX_THREADS) as executor:
                     try:
-                        logging.warning("Updating DB")
+                        # logging.warning("Updating DB")
 
                         for item in res:
-                            print("id: ", item[0])
-                            executor.submit(update_db_with_page_values, cur=cur, page_lines=item[0])
+                            # print(item)
+                            # print("id: ", item[0])
+                            futures.append(executor.submit(update_db_with_page_content, cur=cur, page_lines=item[1]))
+                            # break
+
+                        # for future in concurrent.futures.as_completed(futures):
+                        #     print(future.result())
+                        # print(futures)
+
+                        # this break is to only fetch one page!!!!
+                        # conn.close()
+                        # break
 
                     except (CancelledError, TimeoutError):
+                        logging.warning('Cancelled or Timeout Error')
                         pass
 
+                # 36894 error occurs here
                 cur.execute(CONFIG.config.get_range_pages_from_db, (
                     step,
                     start_id,
@@ -248,9 +357,22 @@ if __name__ == "__main__":
     with open(file='sql_scripts/create_wikipedia_table.sql') as f:
         cur.execute(f.read())
 
+    # if you want to update all wikipedia run this:
     # with open(file='sql_scripts/create_wikipedia_pages_table.sql') as f:
     #     cur.execute(f.read())
+    # insert_pages_to_db(cur)
+
+
+    # main(cur, LIMIT=10000)
     main(cur)
 
     conn.commit()
     conn.close()
+
+# stats
+# insert 10 - 0.03s
+# insert 100 - 0.33s
+# insert 1000 - 3.885619s
+# insert 10000 - 36.372222
+# insert 100000 -
+
